fab56fff2ecee5892adb6157e11c93608feb61f3
Thanawud <thanawud@hotmail.co.th>
2018-09-13 12:38:06 +0700

    commit3

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
+//import CharacterCard from './CharacterCard';
 import WordCard from './WordCard';
 
-const word = "Hello";
 class App extends Component {
-  constructor(props){
- super(props)
- this.state = {
- active: false,
- }
- }
   render() {
     return (
-      <div className="App">
-        <WordCard value="hello"/>
+      <div>
+        {
+          //Array.from(word).map((c, i) => <CharacterCard value={c} key={i} />)
+          <WordCard value="hello"/>
+        }
       </div>
     );
   }
 }
+
 export default App;
